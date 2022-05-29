d4313f7c882b99f5229c5bfbc188bd1e0b0407fa
sergiuRemes <remes.sergiu@gmail.com>
2022-05-29 20:15:22 +0100

    React router app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/:type/:id">
+          <PetDetailsPage />
+        </Route>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+        <Route path="/pet-details-not-found">
+          <PetDetailsNotFound />
+        </Route>
+        <Route path="/:type?">
+          <HomePage />
+        </Route>
+      </Switch>
+
+      <Navigation />
+    </Router>
   );
 }
 
